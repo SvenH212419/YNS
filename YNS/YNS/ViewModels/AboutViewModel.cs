f2dba64657da6ae8e66432a0a8e63b7467d72a32
Mage0Foxy <91876215+Mage0Foxy@users.noreply.github.com>
2022-11-21 13:45:25 +0100

    MongoDB test 0.1.1
    Read may work?

--- YNS/YNS/ViewModels/AboutViewModel.cs
+++ YNS/YNS/ViewModels/AboutViewModel.cs
@@ -2,6 +2,9 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 
 namespace YNS.ViewModels
 {
@@ -11,6 +14,29 @@ namespace YNS.ViewModels
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+
+        }
+        public class MongoDB
+        {
+
+            public MongoDB()
+            {   
+                var settings = MongoClientSettings.FromConnectionString("mongodb+srv://NovaFoxy:<Kyuubi99>@maincluster.jtr1q.mongodb.net/?retryWrites=true&w=majority");
+                settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+                var client = new MongoClient(settings);
+                var database = client.GetDatabase("test");
+
+                var collection = database.GetCollection<BsonDocument>("test");
+
+                Console.WriteLine("Data from MongoDB Database");
+                collection.Find(new BsonDocument()).ForEachAsync(X => Console.WriteLine(X));
+
+                Console.Read();
+
+                OpenConsole = Console.ReadLine();
+            }
+
+            public string OpenConsole { get; }
         }
 
         public ICommand OpenWebCommand { get; }
